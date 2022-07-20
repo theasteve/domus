5f7b15f6cafa73c67913f9365e1444ae99413e20
Steven <stevenaguilarcs@gmail.com>
2022-07-20 08:13:24 -0400

    Create property#new form

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,4 +6,37 @@ class PropertiesController < ApplicationController
   def show
     @property = Property.find(params[:id])
   end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    # TODO: Current user should be property owner.
+    # current_user.properties.create(property_params)
+    @property_owner = PropertyOwner.last
+    @property = @property_owner.properties.create(property_params)
+
+    if @property.save
+      redirect_to @property
+    else
+      Rails.logger.error('Unable to save user')
+      render 'new'
+    end
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(
+      :property_owner_id,
+      :building_name,
+      :address,
+      :city,
+      :state,
+      :zipcode,
+      :country,
+      :photo
+    )
+  end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
   belongs_to :property_owner
   has_many :units
+  has_one_attached :photo
 end
